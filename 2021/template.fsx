1139690aae3521f8e42ee43ddbf95506d46dccd0
Danny Nguyen <danny@80pct.com>
2021-12-14 16:28:29 -0500

    Add template

---
+++ 2021/template.fsx
@@ -0,0 +1,11 @@
+#!/usr/bin/env -S dotnet fsi
+
+open System.IO
+
+let lines = File.ReadLines("data/day3-sample.txt")
+
+let part1 = 0
+let part2 = 0
+
+printfn "Part 1: %A" part1
+printfn "Part 2: %A" part2
