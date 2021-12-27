3540756a31b05665c69c7d5b67fce9ee90a687c0
Danny Nguyen <danny@80pct.com>
2021-12-27 11:50:07 -0500

    Complete Day 7.1

---
+++ 2021/day7.fsx
@@ -0,0 +1,29 @@
+#!/usr/bin/env -S dotnet fsi
+
+open System.IO
+
+let inputLine = File.ReadLines("data/day7-input.txt")
+
+let fuelCost positions x =
+    positions
+    |> Array.map (fun p -> abs (p - x))
+    |> Array.sum
+
+let part1 =
+    let positions =
+        inputLine
+        |> Seq.head
+        |> (fun x -> x.Split ',')
+        |> Array.map int
+
+    let numPos = Array.max positions
+
+    seq { 0..numPos }
+    |> Seq.map (fun n -> fuelCost positions n)
+    |> Seq.min
+
+let part2 = 0
+
+
+printfn "Part 1: %A" part1
+printfn "Part 2: %A" part2
