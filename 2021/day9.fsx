7431e02e00ff99cccac7d6a47310f2741a2fa713
Danny Nguyen <danny@80pct.com>
2021-12-27 23:51:30 -0500

    Complete Day 9.1

--- 2021/day9.fsx
+++ 2021/day9.fsx
@@ -2,7 +2,32 @@
 
 open System.IO
 
-let inputLines = File.ReadLines("data/day9-sample.txt")
+let inputLines = File.ReadLines("data/day9-input.txt")
+
+let adjacentPoints x y map =
+    let maxX = map |> Array2D.length1
+    let maxY = map |> Array2D.length2
+
+    let points =
+        [| (x - 1, y)
+           (x + 1, y)
+           (x, y - 1)
+           (x, y + 1) |]
+
+    points
+    |> Array.filter (fun (x2, y2) -> x2 >= 0 && x2 < maxX && y2 >= 0 && y2 < maxY)
+
+let isLowPoint height adjPoints (map: int [,]) =
+    adjPoints
+    |> Array.forall (fun (x, y) -> map.[x, y] > height)
+
+let riskLevel height adjPoints (map: int [,]) =
+    adjPoints
+    |> Array.forall (fun (x, y) -> map.[x, y] > height)
+    |> (fun x ->
+        match x with
+        | true -> height + 1
+        | false -> 0)
 
 let hmap =
     inputLines
@@ -13,7 +38,12 @@ let hmap =
         |> Seq.map int)
     |> array2D
 
-let part1 = hmap
+let part1 =
+    hmap
+    |> Array2D.mapi (fun x y height -> riskLevel height (adjacentPoints x y hmap) hmap)
+    |> Seq.cast<int>
+    |> Seq.sum
+
 let part2 = 0
 
 printfn "Part 1: %A" part1
