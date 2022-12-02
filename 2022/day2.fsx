c050e77e713e3758e8cc10d64c2b3e5c36d6ba84
Danny Nguyen <danny@keeb.io>
2022-12-02 11:03:06 -0500

    Complete Day 2.2

--- 2022/day2.fsx
+++ 2022/day2.fsx
@@ -10,8 +10,8 @@ type Result =
     | Draw = 3
     | Loss = 0
 
-let codeToMove code =
-    match code with
+let codeToMove =
+    function
     | "A" -> Move.Rock
     | "B" -> Move.Paper
     | "C" -> Move.Scissors
@@ -19,13 +19,18 @@ let codeToMove code =
     | "Y" -> Move.Paper
     | _ -> Move.Scissors
 
+let codeToResult =
+    function
+    | "X" -> Result.Loss
+    | "Y" -> Result.Draw
+    | _ -> Result.Win
+
 let strategyGuide =
     System.IO.File.ReadLines("data/day2-input.txt")
     |> Seq.map (fun line -> line.Split())
-    |> Seq.map (fun line -> line |> Array.map codeToMove)
 
-let roundResult moves =
-    match moves with
+let roundResult =
+    function
     | (x, y) when x = y -> Result.Draw
     | (Move.Rock, Move.Scissors) -> Result.Loss
     | (Move.Paper, Move.Rock) -> Result.Loss
@@ -38,10 +43,26 @@ let roundScore oppMove myMove =
 
 let part1 =
     strategyGuide
+    |> Seq.map (fun line -> line |> Array.map codeToMove)
     |> Seq.map (fun line -> roundScore line[0] line[1])
     |> Seq.sum
 
-let part2 = 0
+let neededMove =
+    function
+    | (x, Result.Draw) -> x
+    | (Move.Rock, Result.Win) -> Move.Paper
+    | (Move.Rock, Result.Loss) -> Move.Scissors
+    | (Move.Scissors, Result.Win) -> Move.Rock
+    | (Move.Scissors, Result.Loss) -> Move.Paper
+    | (Move.Paper, Result.Win) -> Move.Scissors
+    | (Move.Paper, Result.Loss) -> Move.Rock
+    | _ -> Move.Rock
+
+let part2 =
+    strategyGuide
+    |> Seq.map (fun line -> (codeToMove line[0], codeToResult line[1]))
+    |> Seq.map (fun line -> roundScore (fst line) (neededMove line))
+    |> Seq.sum
 
 printfn "Part 1: %A" part1
 printfn "Part 2: %A" part2
