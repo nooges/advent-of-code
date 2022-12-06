b0b0694528cafb3d64af0997dc6ed8ee60450b2e
Danny Nguyen <danny@keeb.io>
2022-12-06 09:49:06 -0500

    Add print utils

--- 2022/Utils.fs
+++ 2022/Utils.fs
@@ -6,3 +6,7 @@ module Utils =
 
     let split c (s: string) = s.Split([| c |])
     let replace (a: string) (b: string) (s: string) = s.Replace(a, b)
+    let pp a = printfn "%A" a
+    let pp1 a = printfn "Part 1: %A" a
+    let pp2 a = printfn "Part 2: %A" a
+    let ps s = printfn "%A" (s |> Seq.toArray)
--- 2022/day6.fsx
+++ 2022/day6.fsx
@@ -1,5 +1,8 @@
 #!/usr/bin/env -S dotnet fsi
 
+#load "Utils.fs"
+open AOC.Utils
+
 let input = System.IO.File.ReadAllText("data/day6-input.txt")
 //let input = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"
 
@@ -8,9 +11,5 @@ let findMarker size =
     >> Seq.findIndex (Set.ofSeq >> Set.count >> (=) size)
     >> (+) size
 
-let part1 = findMarker 4 input
-
-let part2 = findMarker 14 input
-
-printfn "Part 1: %A" part1
-printfn "Part 2: %A" part2
+findMarker 4 input |> pp1
+findMarker 14 input |> pp2
