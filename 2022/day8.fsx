daf01f42ab856579023db056b36b8b02353e02e6
Danny Nguyen <danny@keeb.io>
2022-12-08 13:34:42 -0500

    Refactor look around arrays, Complete Day 8.2

--- 2022/day8.fsx
+++ 2022/day8.fsx
@@ -11,17 +11,26 @@ let grid =
 
 let grid' = grid |> Array.transpose
 
+let lookLeft x y = grid[y] |> Array.take x |> Array.rev
+let lookRight x y = grid[y] |> Array.skip (x + 1)
+let lookUp x y = grid'[x] |> Array.take y |> Array.rev
+let lookDown x y = grid'[x] |> Array.skip (y + 1)
+
+let lookAround =
+    [| lookLeft
+       lookRight
+       lookUp
+       lookDown |]
+
 let isVisible x y value =
     let isMax a =
         match a with
         | [||] -> true
         | a -> Array.max a < value
 
-    // Look left, then right, then up, then down
-    (grid[y] |> Array.take x |> isMax)
-    || (grid[y] |> Array.skip (x + 1) |> isMax)
-    || (grid'[x] |> Array.take y |> isMax)
-    || (grid'[x] |> Array.skip (y + 1) |> isMax)
+    lookAround
+    |> Array.map (fun f -> f x y |> isMax)
+    |> Array.contains true
 
 let countVisible (grid: int [] []) =
     grid
@@ -32,4 +41,24 @@ let countVisible (grid: int [] []) =
         |> Array.length)
     |> Array.sum
 
+let scenicScore x y value =
+    let numViewable a =
+        match a with
+        | [||] -> 0
+        | a when Array.max a < value -> a.Length
+        | _ -> (Array.findIndex (fun x -> x >= value) a) + 1
+
+    lookAround
+    |> Array.map (fun f -> f x y |> numViewable)
+    |> Array.reduce (*)
+
+let highestScenicScore (grid: int [] []) =
+    grid
+    |> Array.mapi (fun y row ->
+        row
+        |> Array.mapi (fun x value -> scenicScore x y value)
+        |> Array.max)
+    |> Array.max
+
 countVisible grid |> pp1
+highestScenicScore grid |> pp2
