98979a84c88aa98496b76832c8d595cf132754cc
Danny Nguyen <danny@keeb.io>
2022-12-08 12:53:29 -0500

    Add more utils

--- 2022/Utils.fs
+++ 2022/Utils.fs
@@ -6,6 +6,9 @@ module Utils =
 
     let split c (s: string) = s.Split([| c |])
     let replace (a: string) (b: string) (s: string) = s.Replace(a, b)
+    let asCharArray (s: string) = s.ToCharArray()
+    let asStringArray: string [] -> string [] = Array.map string
+    let asIntArray: string [] -> int [] = Array.map int
     let pp a = printfn "%A" a
     let pp1 a = printfn "Part 1: %A" a
     let pp2 a = printfn "Part 2: %A" a
