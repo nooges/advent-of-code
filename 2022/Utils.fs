908636fd96582e28893aeb8222ab9da84b467d33
Danny Nguyen <danny@80pct.com>
2022-12-11 13:21:48 -0500

    Complete Day 11.1

--- 2022/Utils.fs
+++ 2022/Utils.fs
@@ -1,10 +1,12 @@
 namespace AOC
 
 open System
+open System.Text.RegularExpressions
 
 module Utils =
 
     let split c (s: string) = s.Split([| c |])
+    let trim (s: string) = s.Trim()
     let replace (a: string) (b: string) (s: string) = s.Replace(a, b)
     let asCharArray (s: string) = s.ToCharArray()
     let asStringArray: string [] -> string [] = Array.map string
@@ -13,3 +15,7 @@ module Utils =
     let pp1 a = printfn "Part 1: %A" a
     let pp2 a = printfn "Part 2: %A" a
     let ps s = printfn "%A" (s |> Seq.toArray)
+
+    let withRegex regex str =
+        [| for m in Regex.Match(str, regex).Groups -> m.Value |]
+        |> Array.tail
---
+++ 2022/day11.fsx
@@ -0,0 +1,110 @@
+#!/usr/bin/env -S dotnet fsi
+
+#load "Utils.fs"
+open AOC.Utils
+
+type Monkey =
+    { Items: int list
+      Operation: int -> int
+      Divisor: int
+      NextTrue: int
+      NextFalse: int
+      Inspected: int }
+
+let input = System.IO.File.ReadAllText("data/day11-sample.txt")
+
+let parseItems =
+    withRegex "  Starting items: (.*)"
+    >> Array.last
+    >> split ','
+    >> Array.map (trim >> int)
+    >> Array.toList
+
+let parseOperation line =
+    let op =
+        line
+        |> withRegex "  Operation: new = old (.*)"
+        |> Array.last
+        |> split ' '
+
+    match op with
+    | [| "*"; "old" |] -> (fun x -> x * x)
+    | [| "*"; n |] -> (*) (int (n))
+    | [| "+"; n |] -> (+) (int (n))
+    | _ -> failwith "Unknown"
+
+let parseMonkey (m: string) =
+    let lastToInt = split ' ' >> Array.last >> int
+    let lines = m.Split '\n'
+
+    { Items = parseItems lines[1]
+      Operation = parseOperation lines[2]
+      Divisor = lines[3] |> lastToInt
+      NextTrue = lines[4] |> lastToInt
+      NextFalse = lines[5] |> lastToInt
+      Inspected = 0 }
+
+let inspectItem op level = (op level) / 3
+
+let addMonkeyItems n newItems (state: Monkey []) =
+    { state[n] with Items = List.append state[n].Items newItems }
+
+let processMonkey n (state: Monkey []) =
+    let m = state[n]
+    let newState = Array.copy state
+
+    m.Items
+    |> List.map (inspectItem m.Operation)
+    |> List.map (fun level ->
+        match level % m.Divisor with
+        | 0 -> (level, m.NextTrue)
+        | _ -> (level, m.NextFalse))
+    |> List.groupBy (fun (l, next) -> next)
+    |> List.map (fun group -> (fst group, snd group |> List.map fst))
+    |> List.iter (fun group ->
+        let next = fst group
+        newState[next] <- addMonkeyItems next (snd group) state)
+
+    newState[n] <- { m with
+        Inspected = m.Inspected + m.Items.Length
+        Items = [] }
+
+    newState
+
+let processRound (state: Monkey []) =
+    let rec loop acc n =
+        match n with
+        | n when n = state.Length -> acc
+        | _ -> loop (processMonkey n acc) (n + 1)
+
+    loop state 0
+
+let runRounds numRounds (state: Monkey []) =
+    let rec loop acc n =
+        match n with
+        | n when n = numRounds -> acc
+        | _ -> loop (processRound acc) (n + 1)
+
+    loop state 0
+
+let initialState =
+    input
+    |> (fun x -> x.Split "\n\n")
+    |> Array.map parseMonkey
+
+//[ 0..2 ] |> List.scan processRound initialState
+
+runRounds 20 initialState
+|> Array.map (fun m -> m.Inspected)
+|> Array.sortDescending
+|> Array.take 2
+|> Array.reduce (*)
+|> pp1
+
+
+runRounds 10000 initialState
+|> Array.map (fun m -> m.Inspected)
+|> Array.sortDescending
+|> Array.take 2
+|> Array.reduce (*)
+|> pp2
