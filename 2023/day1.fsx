2c4663423c7314e3edc966015a3d3ce8731c709e
Danny Nguyen <danny@keeb.io>
2023-12-01 16:44:10 -0500

    Complete Day 1.2

--- 2023/day1.fsx
+++ 2023/day1.fsx
@@ -12,8 +12,38 @@ input
 |> Array.map (fun line ->
     line.ToCharArray()
     |> Array.filter Char.IsDigit
-    |> (fun ds ->
-        10 * (Array.head ds |> charDigitToInt)
-        + (Array.last ds |> charDigitToInt)))
+    |> (fun ds -> 10 * (Array.head ds |> charDigitToInt) + (Array.last ds |> charDigitToInt)))
 |> Array.sum
 |> pp1
+
+let digits =
+    [ "one"; "two"; "three"; "four"; "five"; "six"; "seven"; "eight"; "nine" ]
+    @ List.map string [ 1..9 ]
+
+let findTensDigit s =
+    let rec loop (s: string) pos =
+        let res = digits |> List.tryFindIndex (fun d -> s.Substring(pos).StartsWith(d))
+
+        match res with
+        | Some n -> 10 * ((n % 9) + 1)
+        | _ -> loop s (pos + 1)
+
+    loop s 0
+
+let findOnesDigit s =
+    let rec loop (s: string) pos =
+        let res =
+            digits
+            |> List.tryFindIndex (fun d -> s.Substring(s.Length - pos, pos).StartsWith(d))
+
+        match res with
+        | Some n -> (n % 9) + 1
+        | _ -> loop s (pos + 1)
+
+    loop s 1
+
+// Part 2
+input
+|> Array.map (fun s -> findTensDigit s + findOnesDigit s)
+|> Array.sum
+|> pp2
