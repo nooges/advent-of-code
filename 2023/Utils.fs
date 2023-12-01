f847b532b2c1a411bbad6c7e5b7e30038116731a
Danny Nguyen <danny@keeb.io>
2023-12-01 10:34:54 -0500

    Add utils for 2023

---
+++ 2023/Utils.fs
@@ -0,0 +1,65 @@
+namespace AOC
+
+open System
+open System.Diagnostics
+open System.Text.RegularExpressions
+open System.Collections.Generic
+
+module Utils =
+
+    let split c (s: string) = s.Split([| c |])
+    let splitStr (c: string) (s: string) = s.Split(c)
+    let contains (c: char) (s: string) = s.Contains(c)
+    let trim (s: string) = s.Trim()
+    let replace (a: string) (b: string) (s: string) = s.Replace(a, b)
+    let asCharArray (s: string) = s.ToCharArray()
+    let asStringArray: string [] -> string [] = Array.map string
+    let asIntArray: string [] -> int [] = Array.map int
+    let charToInt (c: char) = int (c) - int 'a'
+    let charDigitToInt (c: char) = int (c) - int '0'
+    let pp a = printfn "%A" a
+
+    let pp1 a =
+        printfn "Part 1:\n-------\n%A\n-------" a
+
+    let pp2 a =
+        printfn "Part 2:\n-------\n%A\n-------" a
+
+    let ps s = printfn "%A" (s |> Seq.toArray)
+
+    let withRegex regex str =
+        [| for m in Regex.Match(str, regex).Groups -> m.Value |]
+        |> Array.tail
+
+    let allInts str =
+        [| for m in Regex.Match(str, """-?\d+""").Groups -> m.Value |]
+        |> Array.tail
+        |> Array.map int
+
+    type Point = { x: int; y: int }
+
+    type TimedOperation<'T> =
+        { millisecondsTaken: int64
+          returnedValue: 'T }
+
+    let timeOperation<'T> (func: unit -> 'T) : TimedOperation<'T> =
+        let timer = new Stopwatch()
+        timer.Start()
+        let returnValue = func ()
+        timer.Stop()
+
+        { millisecondsTaken = timer.ElapsedMilliseconds
+          returnedValue = returnValue }
+
+    let memoize f =
+        let dict = Dictionary<_, _>()
+
+        fun c ->
+            let exist, value = dict.TryGetValue c
+
+            match exist with
+            | true -> value
+            | _ ->
+                let value = f c
+                dict.Add(c, value)
+                value
