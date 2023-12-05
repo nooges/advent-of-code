07cce9249f53a961401902aa97d02bbe10d6d8dc
Danny Nguyen <danny@keeb.io>
2023-12-05 10:16:33 -0500

    Complete Day 5.2

--- 2023/day5.fsx
+++ 2023/day5.fsx
@@ -11,30 +11,48 @@ let seeds = dataGroups[0] |> allInt64s
 
 type categoryMapRange = { dst: int64; src: int64; len: int64 }
 
-type categoryMap =
-    { name: string
-      maps: categoryMapRange[] }
+type categoryMaps = categoryMapRange[]
 
 let parseCategoryMap =
     split '\n'
-    >> (fun a ->
-        { name = a[0]
-          maps =
-            a
-            |> Array.tail
-            |> Array.map (allInt64s >> fun a -> { dst = a[0]; src = a[1]; len = a[2] }) })
+    >> Array.tail
+    >> Array.map (allInt64s >> fun a -> { dst = a[0]; src = a[1]; len = a[2] })
 
 let maps = dataGroups |> Array.tail |> Array.map parseCategoryMap
 
-let mapToNewCategory n (cm: categoryMap) =
-    cm.maps
-    |> Array.tryPick (fun m ->
+let mapToNewCategory n =
+    Array.tryPick (fun m ->
         match n >= m.src && n < (m.src + m.len) with
         | true -> Some(n + m.dst - m.src)
         | _ -> None)
-    |> Option.defaultValue n
+    >> Option.defaultValue n
 
 seeds
 |> Array.map (fun s -> maps |> Array.fold mapToNewCategory s)
 |> Array.min
 |> pp1
+
+// Part 2
+type seedRange = { start: int64; len: int64 }
+
+let seedRanges =
+    seeds
+    |> Array.chunkBySize 2
+    |> Array.map (fun a -> { start = a[0]; len = a[1] })
+
+let inSeedRange n =
+    seedRanges |> Array.exists (fun m -> n >= m.start && n < (m.start + m.len))
+
+let reversedMaps =
+    maps
+    |> Array.map (Array.map (fun m -> { m with dst = m.src; src = m.dst }))
+    |> Array.rev
+
+seedRanges |> pp
+
+Seq.initInfinite (fun n -> n) // Locations
+|> Seq.mapi (fun i s -> (i, reversedMaps |> Array.fold mapToNewCategory s))
+|> Seq.filter (fun (loc, s) -> inSeedRange s)
+|> Seq.head
+|> fst
+|> pp2
