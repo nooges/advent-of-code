b8edfd96f1631b010e415d647ff691b1130bce16
Danny Nguyen <danny@keeb.io>
2023-12-05 11:24:05 -0500

    Add a bit of optimization to speed things up by 2x

--- 2023/day5.fsx
+++ 2023/day5.fsx
@@ -17,15 +17,22 @@ let parseCategoryMap =
     split '\n'
     >> Array.tail
     >> Array.map (allInt64s >> fun a -> { dst = a[0]; src = a[1]; len = a[2] })
+    >> Array.sortBy _.src
 
 let maps = dataGroups |> Array.tail |> Array.map parseCategoryMap
 
-let mapToNewCategory n =
-    Array.tryPick (fun m ->
-        match n >= m.src && n < (m.src + m.len) with
-        | true -> Some(n + m.dst - m.src)
-        | _ -> None)
-    >> Option.defaultValue n
+let mapToNewCategory n ms =
+    let ml = Array.last ms
+
+    match n < ms[0].src || n > (ml.src + ml.len + 1L) with
+    | true -> n
+    | _ ->
+        ms
+        |> Array.tryPick (fun m ->
+            match n >= m.src && n < (m.src + m.len) with
+            | true -> Some(n + m.dst - m.src)
+            | _ -> None)
+        |> Option.defaultValue n
 
 seeds
 |> Array.map (fun s -> maps |> Array.fold mapToNewCategory s)
@@ -39,20 +46,25 @@ let seedRanges =
     seeds
     |> Array.chunkBySize 2
     |> Array.map (fun a -> { start = a[0]; len = a[1] })
+    |> Array.sortBy _.start
+
+let minSeed = seedRanges[0].start
+let maxSeed = (Array.last seedRanges).start + (Array.last seedRanges).len
 
 let inSeedRange n =
-    seedRanges |> Array.exists (fun m -> n >= m.start && n < (m.start + m.len))
+    n >= minSeed
+    && n < maxSeed
+    && seedRanges |> Array.exists (fun m -> n >= m.start && n < (m.start + m.len))
 
 let reversedMaps =
     maps
     |> Array.map (Array.map (fun m -> { m with dst = m.src; src = m.dst }))
     |> Array.rev
-
-seedRanges |> pp
+    |> Array.map (Array.sortBy _.src)
 
 Seq.initInfinite (fun n -> n) // Locations
 |> Seq.mapi (fun i s -> (i, reversedMaps |> Array.fold mapToNewCategory s))
-|> Seq.filter (fun (loc, s) -> inSeedRange s)
+|> Seq.filter (snd >> inSeedRange)
 |> Seq.head
 |> fst
 |> pp2
