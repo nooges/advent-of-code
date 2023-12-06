af8c68bc915fcf3c6043d882adfde35dad34dfd1
Danny Nguyen <danny@keeb.io>
2023-12-06 07:37:14 -0500

    Complete Day 6.2

--- 2023/day6.fsx
+++ 2023/day6.fsx
@@ -6,19 +6,26 @@ open AOC.Utils
 
 let input = System.IO.File.ReadAllLines("data/day6-input.txt")
 
-let times = input[0] |> allInts
-let distances = input[1] |> allInts
+let times = input[0] |> allInt64s
+let distances = input[1] |> allInt64s
 
 let traveled raceTime holdTime = (raceTime - holdTime) * holdTime
 
 // Part 1
 (1, times, distances)
 |||> Array.fold2 (fun acc t d ->
-    [| 0..t |]
-    |> Array.map (traveled t)
-    |> Array.filter (fun x -> x > d)
+    [| 1L .. t |]
+    |> Array.filter (fun i -> (traveled t i) > d)
     |> Array.length
     |> (*) acc)
 |> pp1
 
 // Part 2
+let t2 = input[0].Replace(" ", "") |> allInt64s |> Array.head
+let d2 = input[1].Replace(" ", "") |> allInt64s |> Array.head
+
+timeOperation (fun () ->
+    let start = seq { 1L .. t2 } |> Seq.find (fun n -> traveled t2 n > d2)
+    let stop = seq { 1L .. t2 } |> Seq.findBack (fun n -> traveled t2 n > d2)
+    stop - start + 1L)
+|> pp2
