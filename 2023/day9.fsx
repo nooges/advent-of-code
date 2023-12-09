f842a9da9b8e17ae8c0c82539074ac504a1d6ee0
Danny Nguyen <danny@keeb.io>
2023-12-09 10:48:07 -0500

    Complete Day 9.1

---
+++ 2023/day9.fsx
@@ -0,0 +1,21 @@
+#!/usr/bin/env -S dotnet fsi
+
+#load "Utils.fs"
+
+open AOC.Utils
+open System.Text.RegularExpressions
+
+let input = System.IO.File.ReadAllLines("data/day9-input.txt")
+
+let diff l =
+    l |> Array.pairwise |> Array.map (fun (a, b) -> b - a)
+
+let repeatDiff l =
+    let rec loop acc i s =
+        match Array.forall ((=) 0) s with
+        | true -> acc
+        | _ -> loop (s :: acc) (i + 1) (diff s)
+
+    loop [] 0 l |> List.sumBy Array.last
+
+timeOperation (fun () -> input |> Array.sumBy (allInts >> repeatDiff)) |> pp1
