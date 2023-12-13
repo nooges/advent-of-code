a461f84492e777279aa4dc5567142fe810586a16
Danny Nguyen <danny@keeb.io>
2023-12-13 10:39:23 -0500

    Factor out reflection pair generator

--- 2023/day13.fsx
+++ 2023/day13.fsx
@@ -8,28 +8,27 @@ let input = System.IO.File.ReadAllText("data/day13-input.txt")
 
 let grids = input |> splitStr "\n\n" |> Array.map (splitStr "\n" >> array2D)
 
+
+let reflectionPairs max n =
+    [ 0 .. n - 1 ]
+    |> List.map (fun m -> (n - m - 1, n + m))
+    |> List.filter (fun (a, b) -> a >= 0 && b <= max)
+
 let findReflection grid =
+
     // Search rows
     let maxRow = Array2D.length1 grid - 1
 
     let reflectionRow =
         [ 1..maxRow ]
-        |> List.tryFind (fun n ->
-            [ 0 .. n - 1 ]
-            |> List.map (fun m -> (n - m - 1, n + m))
-            |> List.filter (fun (a, b) -> a >= 0 && b <= maxRow)
-            |> List.forall (fun (a, b) -> grid[a, *] = grid[b, *]))
+        |> List.tryFind (fun n -> reflectionPairs maxRow n |> List.forall (fun (a, b) -> grid[a, *] = grid[b, *]))
 
     // Search cols
     let maxCol = Array2D.length2 grid - 1
 
     let reflectionCol =
         [ 1..maxCol ]
-        |> List.tryFind (fun n ->
-            [ 0 .. n - 1 ]
-            |> List.map (fun m -> (n - m - 1, n + m))
-            |> List.filter (fun (a, b) -> a >= 0 && b <= maxCol)
-            |> List.forall (fun (a, b) -> grid[*, a] = grid[*, b]))
+        |> List.tryFind (fun n -> reflectionPairs maxCol n |> List.forall (fun (a, b) -> grid[*, a] = grid[*, b]))
 
     (reflectionRow, reflectionCol) |>! pp
 
