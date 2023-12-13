75dd9f092ab5e4bb462f5983032189ad47c54036
Danny Nguyen <danny@keeb.io>
2023-12-12 19:54:17 -0500

    Work in progress part 2

--- 2023/day12.fsx
+++ 2023/day12.fsx
@@ -3,6 +3,7 @@
 #load "Utils.fs"
 
 open AOC.Utils
+open System.Collections.Generic
 
 let input = System.IO.File.ReadAllLines("data/day12-input.txt")
 
@@ -17,14 +18,38 @@ let records =
               groups = a[1] |> split ',' |> Array.map int |> Array.toList })
     )
 
-let makePossibleArrangements (condition: string) =
-    let rec loop acc l =
-        match l with
-        | [] -> [ acc ]
-        | '?' :: cs -> loop (acc + ".") cs @ loop (acc + "#") cs
-        | c :: cs -> loop (acc + string c) cs
 
-    loop "" (condition.ToCharArray() |> Array.toList)
+
+let cache = System.Collections.Generic.Dictionary<_, _>()
+
+let makePossibleArrangements totalSprings (condition: string) =
+
+
+    // TODO: move acc to outside and with output of loop, map/concat the list to acc
+    // TODO: loop cache args: # possible springs left, remaining string
+    let rec loop acc springs l =
+        let hash = (acc, springs, l)
+
+        let exist, value = cache.TryGetValue hash
+
+        match exist with
+        | true -> value
+        | _ ->
+            let possibleSpringsLeft =
+                l |> List.filter (fun c -> c = '#' || c = '?') |> List.length
+
+            let value =
+                match l with
+                | [] -> [ acc ]
+                | _ when springs + possibleSpringsLeft < totalSprings -> []
+                | '?' :: cs -> loop (acc + ".") springs cs @ loop (acc + "#") (springs + 1) cs
+                | '#' :: cs -> loop (acc + "#") (springs + 1) cs
+                | c :: cs -> loop (acc + ".") springs cs
+
+            cache.Add(hash, value)
+            value
+
+    loop "" 0 (condition.ToCharArray() |> Array.toList)
 
 let conditionGroups (condition: string) =
     let rec loop acc curr l =
@@ -34,20 +59,38 @@ let conditionGroups (condition: string) =
         | ('#' :: cs, _) -> loop acc (curr + 1) cs
         | ('.' :: cs, 0) -> loop acc 0 cs
         | ('.' :: cs, _) -> loop (curr :: acc) 0 cs
-        | _ -> acc // Shouldn't hit this
+        | _ ->
+            (printfn "bad") |> ignore
+            acc // Shouldn't hit this
 
     loop [] 0 (condition.ToCharArray() |> Array.rev |> Array.toList)
 
-//".??..??...?##." |> makePossibleArrangements |> pp
-//"?????..?#??#?#??###?" |> makePossibleArrangements |> pp
+".??..??...?##." |> makePossibleArrangements 5 |> pp
 
 //"...##.....#..#.##.###." |> conditionGroups |> pp
 timeOperation (fun () ->
     records
     |> Array.sumBy (fun r ->
         r.condition
-        |> makePossibleArrangements
+        |> makePossibleArrangements (r.groups |> List.sum)
         |> List.map conditionGroups
         |> List.filter (fun g -> g = r.groups)
         |> List.length))
 |> pp1
+
+records |> pp
+
+let copies = 2
+
+timeOperation (fun () ->
+    records
+    |> Array.sumBy (fun r ->
+        r.condition
+        |> List.replicate copies
+        |> String.concat "?"
+        |> makePossibleArrangements (r.groups |> List.sum |> (*) copies)
+        |> List.map conditionGroups
+        |> List.filter (fun g -> g = (r.groups |> List.replicate copies |> List.concat))
+        |> List.length
+        |>! pp))
+|> pp2
