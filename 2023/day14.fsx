8e3c9dfab382f23fdfe6f7c0a2740240abd0e32b
Danny Nguyen <danny@keeb.io>
2023-12-15 08:16:28 -0500

    WIP Day 14.2

--- 2023/day14.fsx
+++ 2023/day14.fsx
@@ -4,7 +4,7 @@
 
 open AOC.Utils
 
-let input = System.IO.File.ReadAllLines("data/day14-input.txt")
+let input = System.IO.File.ReadAllLines("data/day14-sample.txt")
 
 let grid = input |> array2D
 
@@ -20,7 +20,59 @@ let calculateLoad (line: char array) =
 
     loop 0 len 0 (line |> Array.toList)
 
+let calculateNewLine (line: char array) =
+    let len = line.Length
+
+    let rec loop acc rockOffset i (l: char list) =
+        match l with
+        | [] ->
+            let numEmpty = i + rockOffset - len
+            acc + (String.replicate numEmpty ".")
+        | '.' :: xs -> loop acc rockOffset (i + 1) xs // Keep going
+        | 'O' :: xs -> loop (acc + "O") (rockOffset - 1) (i + 1) xs // Push onto accString
+        | _ :: xs ->
+            let numEmpty = i + rockOffset - len
+            let acc' = acc + (String.replicate numEmpty ".") + "#"
+            loop acc' (len - i - 1) (i + 1) xs // Fill rest of accString up to new offset with '.', and also push '#'
+
+    loop "" len 0 (line |> Array.toList)
+
+let revString s =
+    new string (s |> Seq.rev |> Seq.toArray)
+
+let tiltUpWithTranspose g =
+    [ 0 .. (g |> Array2D.length2) - 1 ]
+    |> List.map (fun col -> g[*, col] |> calculateNewLine)
+    |> array2D
+
+let tiltDownWithTranspose g =
+    [ 0 .. (g |> Array2D.length2) - 1 ]
+    |> List.map (fun col -> g[*, col] |> Array.rev |> calculateNewLine |> revString)
+    |> array2D
+
+let totalLoad g =
+    [ 0 .. (g |> Array2D.length1) - 1 ]
+    |> List.sumBy (fun row ->
+        g[row, *]
+        |> Array.filter ((=) 'O')
+        |> Array.length
+        |> (*) (Array2D.length2 g - row))
+    |>! pp
+
+let cycle =
+    tiltUpWithTranspose
+    >> tiltUpWithTranspose
+    >> tiltDownWithTranspose
+    >> tiltDownWithTranspose
+
 timeOperation (fun () ->
     [ 0 .. (grid |> Array2D.length2) - 1 ]
     |> List.sumBy (fun col -> grid[*, col] |> calculateLoad))
 |> pp1
+
+let numCycles = 100
+
+timeOperation (fun () -> [ 1..numCycles ] |> List.fold (fun g _ -> cycle g) grid |> totalLoad)
+|> pp2
+
+//cycle grid |> cycle |> pp
