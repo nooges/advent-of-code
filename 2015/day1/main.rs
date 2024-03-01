9d7dd5753474426e6750c205069b229b9f4ea3f8
Danny Nguyen <danny@keeb.io>
2024-03-01 09:51:32 -0500

    Complete 2015 Day 1.2

--- 2015/day1/main.rs
+++ 2015/day1/main.rs
@@ -5,9 +5,22 @@ fn part1(input: &str) -> i32 {
     return 2 * open_chars - input.len() as i32;
 }
 
+fn part2(input: &str) -> i32 {
+    let mut floor = 0;
+    for (i, c) in input.chars().enumerate() {
+        floor += if c == '(' { 1 } else { -1 };
+        if floor < 0 {
+            return i as i32 + 1;
+        }
+    }
+    return floor;
+}
+
 fn main() -> std::io::Result<()> {
     let input = fs::read_to_string("input.txt")?;
     let part1_result = part1(&input);
     println!("Part 1: {}", part1_result);
+    let part2_result = part2(&input);
+    println!("Part 2: {}", part2_result);
     Ok(())
 }
