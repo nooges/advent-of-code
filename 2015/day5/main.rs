c9771fb91ec72c1bdc34442f46186f56fd276c72
Danny Nguyen <danny@keeb.io>
2024-03-01 16:50:49 -0500

    Complete 2015.5.2

--- 2015/day5/main.rs
+++ 2015/day5/main.rs
@@ -33,8 +33,22 @@ fn part1(input: &str) -> u32 {
     return input.lines().filter(|l| rules.iter().all(|f| f(l))).count() as u32;
 }
 
+fn has_non_overlapping_pairs(s: &str) -> bool {
+    for i in 0..s.len() - 2 {
+        if s[i + 2..].contains(&s[i..i + 2]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+fn has_sandwiched_letter(s: &str) -> bool {
+    return s.chars().skip(2).zip(s.chars()).any(|(a, b)| a == b);
+}
+
 fn part2(input: &str) -> u32 {
-    return 0;
+    let rules = [has_non_overlapping_pairs, has_sandwiched_letter];
+    return input.lines().filter(|l| rules.iter().all(|f| f(l))).count() as u32;
 }
 
 fn main() -> std::io::Result<()> {
