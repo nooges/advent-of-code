a7e2fc71ad583a8fe9564177f588d22f6c4714df
Danny Nguyen <danny@keeb.io>
2024-03-01 13:43:36 -0500

    Complete 2015.4.1

---
+++ 2015/day4/main.rs
@@ -0,0 +1,25 @@
+use std::fs;
+
+fn md5hash(key: &str, num: u32) -> String {
+    let res = md5::compute(format!("{}{}", key, num));
+    return format!("{:x}", res);
+}
+
+fn part1(input: &str) -> u32 {
+    return (0..)
+        .find(|n| md5hash(input, *n).starts_with("00000"))
+        .unwrap();
+}
+
+fn part2(input: &str) -> u32 {
+    return 0;
+}
+
+fn main() -> std::io::Result<()> {
+    let input = fs::read_to_string("input.txt")?;
+    let part1_result = part1(&input);
+    println!("Part 1: {}", part1_result);
+    let part2_result = part2(&input);
+    println!("Part 2: {}", part2_result);
+    Ok(())
+}
