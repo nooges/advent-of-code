bf871d57d90d8f976e244a5dd06fe5366d399dd8
Danny Nguyen <danny@keeb.io>
2024-03-12 16:40:16 -0400

    Use regex to extract ints

--- 2015/utils/src/lib.rs
+++ 2015/utils/src/lib.rs
@@ -1,3 +1,5 @@
+use regex::Regex;
+
 pub fn timeit<F: Fn() -> u32>(name: &str, f: F) {
     let start = std::time::Instant::now();
     let result = f();
@@ -9,8 +11,16 @@ pub fn timeit<F: Fn() -> u32>(name: &str, f: F) {
     }
 }
 
-pub fn extract_u32s(line: &str) -> Vec<u32> {
-    line.split_whitespace()
-        .filter_map(|s| s.parse::<u32>().ok())
+pub fn extract_u32s(input: &str) -> Vec<u32> {
+    let re = Regex::new(r"?\d+").unwrap();
+    re.find_iter(input)
+        .map(|m| m.as_str().parse().unwrap())
+        .collect()
+}
+
+pub fn extract_i32s(input: &str) -> Vec<i32> {
+    let re = Regex::new(r"-?\d+").unwrap();
+    re.find_iter(input)
+        .map(|m| m.as_str().parse().unwrap())
         .collect()
 }
