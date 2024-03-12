c90386d11f480cde1c0b87973db7ba075e3010c8
Danny Nguyen <danny@keeb.io>
2024-03-12 12:19:01 -0400

    Complete 2015.14.2

--- 2015/day14/main.rs
+++ 2015/day14/main.rs
@@ -21,23 +21,40 @@ fn parse_input(input: &str) -> Vec<Reindeer> {
         .collect()
 }
 
+fn position(r: &Reindeer, t: u32) -> u32 {
+    let cycles = t / (r.move_time + r.rest_time);
+    cycles * r.move_time * r.speed + min(r.move_time, t % (r.move_time + r.rest_time)) * r.speed
+}
+
 fn part1(input: &str) -> u32 {
     let race_time = 2503;
     parse_input(input)
         .iter()
-        .map(|r| {
-            let cycles = race_time / (r.move_time + r.rest_time);
-            cycles * r.move_time * r.speed
-                + min(r.move_time, race_time % (r.move_time + r.rest_time)) * r.speed
-        })
+        .map(|r| position(r, race_time))
         .max()
         .unwrap()
 }
 
 fn part2(input: &str) -> u32 {
-    let reindeers = parse_input(input);
-    //dbg!(reindeers);
-    0
+    let race_time = 2503;
+    let reindeer = parse_input(input);
+    let distances = reindeer
+        .iter()
+        .map(|r| (1..=race_time).map(|t| position(r, t)).collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+    let max_distances = (0..distances[0].len())
+        .map(|i| distances.iter().map(|d| d[i]).max().unwrap())
+        .collect::<Vec<_>>();
+    distances
+        .iter()
+        .map(|d| {
+            d.iter()
+                .zip(max_distances.iter())
+                .filter(|(a, b)| a == b)
+                .count()
+        })
+        .max()
+        .unwrap() as u32
 }
 
 fn main() -> std::io::Result<()> {
