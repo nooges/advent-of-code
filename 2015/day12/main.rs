3a45c10c834ac7d8d1c6f8faa22926591fa5e27b
Danny Nguyen <danny@keeb.io>
2024-03-17 19:06:52 -0400

    Complete 2015.12.1

---
+++ 2015/day12/main.rs
@@ -0,0 +1,17 @@
+use std::fs;
+
+fn part1(input: &str) -> u32 {
+    utils::extract_i32s(input).iter().sum::<i32>() as u32
+}
+
+fn part2(input: &str) -> u32 {
+    0
+}
+
+fn main() -> std::io::Result<()> {
+    let input = fs::read_to_string("input.txt")?;
+    utils::timeit("Part 1", || part1(&input));
+    utils::timeit("Part 2", || part2(&input));
+
+    Ok(())
+}
