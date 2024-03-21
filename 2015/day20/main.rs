158d11537fd84a335be23b19dbdf600160104787
Danny Nguyen <danny@keeb.io>
2024-03-21 06:34:27 -0400

    Complete 2015.20.1

---
+++ 2015/day20/main.rs
@@ -0,0 +1,24 @@
+use std::fs;
+
+fn part1(input: &str) -> u32 {
+    let num_presents_limit: u32 = input.parse().unwrap();
+    for house in 0.. {
+        let presents: u32 = (1..=house).filter(|elf| house % elf == 0).sum::<u32>() * 10;
+        if presents >= num_presents_limit {
+            return house;
+        }
+    }
+    0
+}
+
+fn part2(input: &str) -> u32 {
+    0
+}
+
+fn main() -> std::io::Result<()> {
+    let input = fs::read_to_string("input.txt")?;
+    utils::timeit("Part 1", || part1(&input));
+    utils::timeit("Part 2", || part2(&input));
+
+    Ok(())
+}
