f1e9ba81c85bb4280ecf48338cf24b08bee85cdb
Danny Nguyen <danny@keeb.io>
2024-12-01 18:10:59 -0500

    Complete 2024.1.2

--- 2024/day1/main.rs
+++ 2024/day1/main.rs
@@ -1,12 +1,12 @@
 use itertools::Itertools;
 
-fn dist(a: i32, b: i32) -> u32 {
-    (a - b).unsigned_abs()
+fn dist(a: u32, b: u32) -> u32 {
+    (a as i32 - b as i32).unsigned_abs()
 }
 
-fn part1(pairs: &Vec<Vec<i32>>) -> u32 {
-    let col1: Vec<i32> = pairs.iter().map(|p| p[0]).sorted().collect();
-    let col2: Vec<i32> = pairs.iter().map(|p| p[1]).sorted().collect();
+fn part1(pairs: &[Vec<u32>]) -> u32 {
+    let col1: Vec<u32> = pairs.iter().map(|p| p[0]).sorted().collect();
+    let col2: Vec<u32> = pairs.iter().map(|p| p[1]).sorted().collect();
 
     col1.iter()
         .zip(col2.iter())
@@ -14,12 +14,19 @@ fn part1(pairs: &Vec<Vec<i32>>) -> u32 {
         .sum()
 }
 
+fn similarity(n: u32, v: &[u32]) -> u32 {
+    v.iter().filter(|&x| *x == n).count() as u32 * n
+}
+
+fn part2(pairs: &[Vec<u32>]) -> u32 {
+    let col2: Vec<u32> = pairs.iter().map(|p| p[1]).collect();
+    pairs.iter().map(|p| similarity(p[0], &col2)).sum()
+}
+
 fn main() -> std::io::Result<()> {
     let input = include_str!("input.txt");
-    let pairs = input
-        .lines()
-        .map(|line| aoc2024_utils::extract_i32s(line))
-        .collect();
+    let pairs: Vec<Vec<u32>> = input.lines().map(aoc2024_utils::extract_u32s).collect();
     aoc2024_utils::timeit("Part 1", || part1(&pairs));
+    aoc2024_utils::timeit("Part 2", || part2(&pairs));
     Ok(())
 }
