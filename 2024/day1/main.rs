ffecf70e2d602be49a82e858d5a707ec0f2b05e6
Danny Nguyen <danny@keeb.io>
2024-12-01 17:55:22 -0500

    Complete 2024.1.1

---
+++ 2024/day1/main.rs
@@ -0,0 +1,25 @@
+use itertools::Itertools;
+
+fn dist(a: i32, b: i32) -> i32 {
+    (a - b).abs()
+}
+
+fn part1(pairs: &Vec<Vec<i32>>) -> i32 {
+    let col1: Vec<i32> = pairs.iter().map(|p| p[0]).sorted().collect();
+    let col2: Vec<i32> = pairs.iter().map(|p| p[1]).sorted().collect();
+
+    col1.iter()
+        .zip(col2.iter())
+        .map(|(a, b)| dist(*a, *b))
+        .sum()
+}
+
+fn main() -> std::io::Result<()> {
+    let input = include_str!("input.txt");
+    let pairs = input
+        .lines()
+        .map(|line| aoc2024_utils::extract_i32s(line))
+        .collect();
+    aoc2024_utils::timeit("Part 1", || part1(&pairs) as u32);
+    Ok(())
+}
---
+++ 2024/utils/src/lib.rs
@@ -0,0 +1,26 @@
+use regex::Regex;
+
+pub fn timeit<F: Fn() -> u32>(name: &str, f: F) {
+    let start = std::time::Instant::now();
+    let result = f();
+    let elapsed = start.elapsed();
+    if elapsed.as_millis() > 1 {
+        println!("{}: {} \tTime: {}ms", name, result, elapsed.as_millis());
+    } else {
+        println!("{}: {} \tTime: {}μs", name, result, elapsed.as_micros());
+    }
+}
+
+pub fn extract_u32s(input: &str) -> Vec<u32> {
+    let re = Regex::new(r"\d+").unwrap();
+    re.find_iter(input)
+        .map(|m| m.as_str().parse().unwrap())
+        .collect()
+}
+
+pub fn extract_i32s(input: &str) -> Vec<i32> {
+    let re = Regex::new(r"-?\d+").unwrap();
+    re.find_iter(input)
+        .map(|m| m.as_str().parse().unwrap())
+        .collect()
+}
