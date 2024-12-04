21888667eeff546612c90fadd587e74b978af2f5
Danny Nguyen <danny@keeb.io>
2024-12-04 11:09:17 -0500

    Update timer threshold for showing ms

--- 2024/utils/src/lib.rs
+++ 2024/utils/src/lib.rs
@@ -4,7 +4,7 @@ pub fn timeit<F: Fn() -> u32>(name: &str, f: F) {
     let start = std::time::Instant::now();
     let result = f();
     let elapsed = start.elapsed();
-    if elapsed.as_millis() > 1 {
+    if elapsed.as_millis() > 10 {
         println!("{}: {} \tTime: {}ms", name, result, elapsed.as_millis());
     } else {
         println!("{}: {} \tTime: {}μs", name, result, elapsed.as_micros());
