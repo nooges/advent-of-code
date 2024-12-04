4261164f24e2c25321b502feb1676e38853c6c5d
Danny Nguyen <danny@keeb.io>
2024-12-04 11:09:34 -0500

    Some cleanup

--- 2024/day4/main.rs
+++ 2024/day4/main.rs
@@ -1,38 +1,31 @@
-fn look(
-    look_dirs: &Vec<Vec<(usize, usize)>>,
-    search: &str,
-    r: usize,
-    c: usize,
-    rows: &[&str],
-) -> u32 {
+fn look(look_dirs: &[Vec<(usize, usize)>], search: &str, r: usize, c: usize, rows: &[&str]) -> u32 {
     let ncols = rows[0].len();
     let nrows = rows.len();
-    let search_bytes = search.as_bytes();
-    let search_rev_bytes: Vec<u8> = search_bytes.iter().rev().copied().collect();
+    let search_fwd = search.as_bytes();
+    let search_rev = &search_fwd.iter().rev().copied().collect::<Vec<_>>();
 
     look_dirs
         .iter()
         .map(|dirs| {
             dirs.iter()
                 .map(|(dr, dc)| {
-                    if r + dr < nrows && c + dc < ncols {
-                        rows[r + dr].as_bytes()[c + dc]
+                    let (nr, nc) = (r + dr, c + dc);
+                    if nr < nrows && nc < ncols {
+                        rows[nr].as_bytes()[nc]
                     } else {
                         b' '
                     }
                 })
                 .collect::<Vec<u8>>()
         })
-        .filter(|s| s == search_bytes || s == &search_rev_bytes)
+        .filter(|s| s == search_fwd || s == search_rev)
         .count() as u32
 }
 
 fn part1(input: &str) -> u32 {
     let rows = input.lines().collect::<Vec<_>>();
-    let ncols = rows[0].len();
-    let nrows = rows.len();
 
-    let look_dirs = vec![
+    let look_dirs = [
         vec![(0, 0), (0, 1), (0, 2), (0, 3)], // horizontal
         vec![(0, 0), (1, 0), (2, 0), (3, 0)], // vertical
         vec![(0, 0), (1, 1), (2, 2), (3, 3)], // diagonal down
@@ -40,9 +33,9 @@ fn part1(input: &str) -> u32 {
     ];
     let search = "XMAS";
 
-    (0..ncols)
+    (0..rows[0].len())
         .map(|c| {
-            (0..nrows)
+            (0..rows.len())
                 .map(|r| look(&look_dirs, search, r, c, &rows[..]))
                 .sum::<u32>()
         })
@@ -51,18 +44,16 @@ fn part1(input: &str) -> u32 {
 
 fn part2(input: &str) -> u32 {
     let rows = input.lines().collect::<Vec<_>>();
-    let ncols = rows[0].len();
-    let nrows = rows.len();
 
-    let look_dirs = vec![
+    let look_dirs = [
         vec![(0, 0), (1, 1), (2, 2)], // diagonal down
         vec![(2, 0), (1, 1), (0, 2)], // diagonal up
     ];
     let search = "MAS";
 
-    (0..ncols)
+    (0..rows[0].len())
         .map(|c| {
-            (0..nrows)
+            (0..rows.len())
                 .filter(|&r| look(&look_dirs, search, r, c, &rows[..]) == 2)
                 .count()
         })
@@ -71,7 +62,7 @@ fn part2(input: &str) -> u32 {
 
 fn main() -> std::io::Result<()> {
     let input = include_str!("input.txt");
-    aoc2024_utils::timeit("Part 1", || part1(&input));
-    aoc2024_utils::timeit("Part 2", || part2(&input));
+    aoc2024_utils::timeit("Part 1", || part1(input));
+    aoc2024_utils::timeit("Part 2", || part2(input));
     Ok(())
 }
