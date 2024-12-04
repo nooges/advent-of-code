d1b84c067eac6d6d08395576b348e44c7cdd2b2b
Danny Nguyen <danny@keeb.io>
2024-12-04 10:10:00 -0500

    Complete 2024.4.1

---
+++ 2024/day4/main.rs
@@ -0,0 +1,43 @@
+fn look_for_xmas(r: usize, c: usize, rows: &[&str]) -> u32 {
+    let look_dirs = [
+        [(0, 0), (0, 1), (0, 2), (0, 3)], // horizontal
+        [(0, 0), (1, 0), (2, 0), (3, 0)], // vertical
+        [(0, 0), (1, 1), (2, 2), (3, 3)], // diagonal down
+        [(3, 0), (2, 1), (1, 2), (0, 3)], // diagonal up
+    ];
+    let ncols = rows[0].len();
+    let nrows = rows.len();
+
+    look_dirs
+        .iter()
+        .map(|dirs| {
+            dirs.iter()
+                .map(|(dr, dc)| {
+                    if r + dr < nrows && c + dc < ncols {
+                        rows[r + dr].as_bytes()[c + dc]
+                    } else {
+                        b' '
+                    }
+                })
+                .collect::<Vec<u8>>()
+        })
+        .filter(|s| s == "XMAS".as_bytes() || s == "SAMX".as_bytes())
+        .count() as u32
+}
+
+fn part1(input: &str) -> u32 {
+    let rows = input.lines().collect::<Vec<_>>();
+    let ncols = rows[0].len();
+    let nrows = rows.len();
+
+    (0..ncols)
+        .map(|c| (0..nrows).map(|r| look_for_xmas(r, c, &rows)).sum::<u32>())
+        .sum()
+}
+
+fn main() -> std::io::Result<()> {
+    let input = include_str!("input.txt");
+    aoc2024_utils::timeit("Part 1", || part1(&input));
+    //aoc2024_utils::timeit("Part 2", || part2(&input));
+    Ok(())
+}
