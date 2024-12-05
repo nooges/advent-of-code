f203004059cb4386ad4c5376fcdf94fa6f5211d5
Danny Nguyen <danny@keeb.io>
2024-12-05 08:40:14 -0500

    Complete 2024.5.2

--- 2024/day5/main.rs
+++ 2024/day5/main.rs
@@ -1,3 +1,5 @@
+use itertools::Itertools;
+
 fn part1(input: &str) -> u32 {
     let (rules, updates) = input.split_once("\n\n").unwrap();
     updates
@@ -13,9 +15,35 @@ fn part1(input: &str) -> u32 {
         .sum()
 }
 
+fn part2(input: &str) -> u32 {
+    let (rules, updates) = input.split_once("\n\n").unwrap();
+    updates
+        .lines()
+        .map(|l| l.split(",").collect::<Vec<_>>())
+        .filter(|pages| {
+            pages
+                .windows(2)
+                .any(|w| !rules.contains(&format!("{}|{}", w[0], w[1])))
+        })
+        .map(|pages| {
+            let sorted_pages = pages
+                .iter()
+                .sorted_by(|a, b| {
+                    if rules.contains(&format!("{a}|{b}")) {
+                        std::cmp::Ordering::Less
+                    } else {
+                        std::cmp::Ordering::Greater
+                    }
+                })
+                .collect::<Vec<_>>();
+            sorted_pages[pages.len() / 2].parse::<u32>().unwrap()
+        })
+        .sum()
+}
+
 fn main() -> std::io::Result<()> {
-    let input = include_str!("input.txt");
+    let input = include_str!("sample.txt");
     aoc2024_utils::timeit("Part 1", || part1(input));
-    //aoc2024_utils::timeit("Part 2", || part2(input));
+    aoc2024_utils::timeit("Part 2", || part2(input));
     Ok(())
 }
