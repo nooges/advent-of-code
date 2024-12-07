0a466864cfd2b7f8e8e9288824489846e4b4ba62
Danny Nguyen <danny@keeb.io>
2024-12-07 11:07:56 -0500

    Complete 2024.7.2

--- 2024/day7/main.rs
+++ 2024/day7/main.rs
@@ -20,8 +20,31 @@ fn parse_input(input: &str) -> Vec<Equation> {
         .collect_vec()
 }
 
+fn concat(a: u64, b: u64) -> u64 {
+    format!("{}{}", a, b).parse().unwrap()
+}
+
 fn equation_possible(test_value: u64, items: &[u64]) -> bool {
     let possible_ops: [fn(u64, u64) -> u64; 2] = [u64::add, u64::mul];
+    let combinations = std::iter::repeat(possible_ops)
+        .take(items.len() - 1)
+        .multi_cartesian_product();
+
+    for combination in combinations {
+        let result = combination
+            .into_iter()
+            .zip(items.iter().skip(1))
+            .fold(items[0], |acc, (op, item)| op(acc, *item));
+        if result == test_value {
+            return true;
+        }
+    }
+
+    false
+}
+
+fn equation_possible_2(test_value: u64, items: &[u64]) -> bool {
+    let possible_ops: [fn(u64, u64) -> u64; 3] = [u64::add, u64::mul, concat];
     let combinations = std::iter::repeat(possible_ops)
         .take(items.len() - 1)
         .multi_cartesian_product();
@@ -48,9 +71,18 @@ fn part1(input: &str) -> u64 {
         .sum()
 }
 
+fn part2(input: &str) -> u64 {
+    let equations = parse_input(input);
+    equations
+        .iter()
+        .filter(|eq| equation_possible_2(eq.test_value, &eq.items))
+        .map(|eq| eq.test_value)
+        .sum()
+}
+
 fn main() -> std::io::Result<()> {
     let input = include_str!("input.txt");
     aoc2024_utils::timeit_u64("Part 1", || part1(input));
-    //aoc2024_utils::timeit("Part 2", || part2(input));
+    aoc2024_utils::timeit_u64("Part 2", || part2(input));
     Ok(())
 }
