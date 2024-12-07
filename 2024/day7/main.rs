afd65317d2fd74dac1f3e120480aed53818c2b07
Danny Nguyen <danny@keeb.io>
2024-12-07 14:04:47 -0500

    Rename operands

--- 2024/day7/main.rs
+++ 2024/day7/main.rs
@@ -5,7 +5,7 @@ use std::ops::{Add, Mul};
 #[derive(Debug)]
 struct Equation {
     test_value: u64,
-    items: Vec<u64>,
+    operands: Vec<u64>,
 }
 
 fn parse_input(input: &str) -> Vec<Equation> {
@@ -15,7 +15,7 @@ fn parse_input(input: &str) -> Vec<Equation> {
             let (h, t) = l.split_once(": ").unwrap();
             Equation {
                 test_value: h.parse().unwrap(),
-                items: aoc2024_utils::extract_u64s(t),
+                operands: aoc2024_utils::extract_u64s(t),
             }
         })
         .collect_vec()
@@ -25,14 +25,18 @@ fn concat(a: u64, b: u64) -> u64 {
     format!("{}{}", a, b).parse().unwrap()
 }
 
-fn equation_possible(test_value: u64, items: &[u64], possible_ops: &[fn(u64, u64) -> u64]) -> bool {
+fn equation_possible(
+    test_value: u64,
+    operands: &[u64],
+    possible_ops: &[fn(u64, u64) -> u64],
+) -> bool {
     let mut combinations = std::iter::repeat(possible_ops)
-        .take(items.len() - 1)
+        .take(operands.len() - 1)
         .multi_cartesian_product();
 
     combinations.any(|combination| {
-        let mut result = items[0];
-        for (op, item) in combination.iter().zip(items.iter().skip(1)) {
+        let mut result = operands[0];
+        for (op, item) in combination.iter().zip(operands.iter().skip(1)) {
             result = op(result, *item);
             if result > test_value {
                 return false;
@@ -46,7 +50,7 @@ fn part1(input: &str) -> u64 {
     let possible_ops: [fn(u64, u64) -> u64; 2] = [u64::add, u64::mul];
     parse_input(input)
         .iter()
-        .filter(|eq| equation_possible(eq.test_value, &eq.items, &possible_ops))
+        .filter(|eq| equation_possible(eq.test_value, &eq.operands, &possible_ops))
         .map(|eq| eq.test_value)
         .sum()
 }
@@ -55,7 +59,7 @@ fn part2(input: &str) -> u64 {
     let possible_ops: [fn(u64, u64) -> u64; 3] = [u64::add, u64::mul, concat];
     parse_input(input)
         .par_iter()
-        .filter(|eq| equation_possible(eq.test_value, &eq.items, &possible_ops))
+        .filter(|eq| equation_possible(eq.test_value, &eq.operands, &possible_ops))
         .map(|eq| eq.test_value)
         .sum()
 }
