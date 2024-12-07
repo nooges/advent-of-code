e6f176e43b805c358ce58b93412d3edb1c7bdd9b
Danny Nguyen <danny@keeb.io>
2024-12-07 11:04:03 -0500

    Add u64 version of some utils

--- 2024/utils/src/lib.rs
+++ 2024/utils/src/lib.rs
@@ -11,6 +11,17 @@ pub fn timeit<F: Fn() -> u32>(name: &str, f: F) {
     }
 }
 
+pub fn timeit_u64<F: Fn() -> u64>(name: &str, f: F) {
+    let start = std::time::Instant::now();
+    let result = f();
+    let elapsed = start.elapsed();
+    if elapsed.as_millis() > 10 {
+        println!("{}: {} \tTime: {}ms", name, result, elapsed.as_millis());
+    } else {
+        println!("{}: {} \tTime: {}μs", name, result, elapsed.as_micros());
+    }
+}
+
 pub fn extract_u32s(input: &str) -> Vec<u32> {
     let re = Regex::new(r"\d+").unwrap();
     re.find_iter(input)
@@ -20,6 +31,13 @@ pub fn extract_u32s(input: &str) -> Vec<u32> {
 
 pub fn extract_i32s(input: &str) -> Vec<i32> {
     let re = Regex::new(r"-?\d+").unwrap();
+    re.find_iter(input)
+        .map(|m| m.as_str().parse().unwrap())
+        .collect()
+}
+
+pub fn extract_u64s(input: &str) -> Vec<u64> {
+    let re = Regex::new(r"\d+").unwrap();
     re.find_iter(input)
         .map(|m| m.as_str().parse().unwrap())
         .collect()
