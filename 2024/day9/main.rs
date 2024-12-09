aa66d146e9a71b07a4028fe9b2799c2820b39c28
Danny Nguyen <danny@keeb.io>
2024-12-09 11:01:54 -0500

    Complete 2024.9.1

---
+++ 2024/day9/main.rs
@@ -0,0 +1,48 @@
+use itertools::Itertools;
+
+fn part1(input: &str) -> u64 {
+    let mut disk: Vec<i64> = Vec::with_capacity(100000);
+    let mut chars = input.chars().collect_vec();
+    chars.push('0');
+
+    let diskmap = chars
+        .iter()
+        .map(|c| c.to_digit(10).unwrap() as usize)
+        .tuples::<(usize, usize)>()
+        .collect_vec();
+    diskmap.iter().enumerate().for_each(|(id, (len, space))| {
+        disk.extend(vec![id as i64; *len]);
+        disk.extend(vec![-1; *space]);
+    });
+
+    // Rearrange
+    let mut cur_l = diskmap[0].0;
+    let mut cur_r = disk.len() - 1;
+    loop {
+        while disk[cur_l] != -1 {
+            cur_l += 1;
+        }
+        while disk[cur_r] == -1 {
+            cur_r -= 1;
+        }
+        if cur_l > cur_r {
+            break;
+        }
+        println!("{} {}", cur_l, cur_r);
+        disk.swap(cur_l, cur_r);
+        cur_l += 1;
+        cur_r -= 1;
+    }
+
+    (0..cur_r + 1)
+        .map(|i| i as u64 * disk[i] as u64)
+        .sum::<u64>()
+}
+
+fn main() -> std::io::Result<()> {
+    let input = include_str!("input.txt");
+
+    aoc2024_utils::timeit_u64("Part 1", || part1(input));
+    //aoc2024_utils::timeit_u64("Part 2", || part2(input));
+    Ok(())
+}
