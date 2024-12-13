7c904f2deef38a6ee5ee6ba4c2fe0c3b6446c5a3
Danny Nguyen <danny@keeb.io>
2024-12-13 10:47:48 -0500

    nalgebra version

--- 2024/day13/main.rs
+++ 2024/day13/main.rs
@@ -1,4 +1,5 @@
-use faer::prelude::*;
+extern crate nalgebra as na;
+use na::{Matrix2, Matrix2x1};
 
 fn is_close(n: f64) -> bool {
     (n - n.round()).abs() < 1e-4
@@ -12,11 +13,10 @@ fn solve(input: &str, offset: f64) -> u64 {
                 .lines()
                 .map(aoc2024_utils::extract_u64s)
                 .collect::<Vec<_>>();
-            let a = Mat::from_fn(2, 2, |i, j| data[j][i] as f64);
-            let b = Mat::from_fn(2, 1, |i, _| offset + data[2][i] as f64);
-            let plu = a.partial_piv_lu();
-            let x = plu.solve(&b);
-            x.col_as_slice(0).to_vec()
+            let a = Matrix2::from_fn(|i, j| data[j][i] as f64);
+            let b = Matrix2x1::from_fn(|i, _| offset + data[2][i] as f64);
+            let x = a.lu().solve(&b).unwrap();
+            x.column(0).as_slice().to_vec()
         })
         .filter(|x| is_close(x[0]) && is_close(x[1]))
         .map(|x| 3 * x[0].round() as u64 + x[1].round() as u64)
