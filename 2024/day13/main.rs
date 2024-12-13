d86db4caad5c55685c80deff23d0ab8a963c9b5d
Danny Nguyen <danny@keeb.io>
2024-12-13 09:20:03 -0500

    Cleanup

--- 2024/day13/main.rs
+++ 2024/day13/main.rs
@@ -8,19 +8,18 @@ fn solve(input: &str, offset: f64) -> u64 {
     input
         .split("\n\n")
         .map(|m| {
-            m.lines()
+            let data = m
+                .lines()
                 .map(aoc2024_utils::extract_u64s)
-                .collect::<Vec<_>>()
-        })
-        .map(|m| {
-            let a = Mat::from_fn(2, 2, |i, j| m[j][i] as f64);
-            let b = Mat::from_fn(2, 1, |i, _| offset + m[2][i] as f64);
+                .collect::<Vec<_>>();
+            let a = Mat::from_fn(2, 2, |i, j| data[j][i] as f64);
+            let b = Mat::from_fn(2, 1, |i, _| offset + data[2][i] as f64);
             let plu = a.partial_piv_lu();
             let x = plu.solve(&b);
-            (*x.get(0, 0), *x.get(1, 0))
+            x.col_as_slice(0).to_vec()
         })
-        .filter(|(a, b)| is_close(*a) && is_close(*b))
-        .map(|(a, b)| 3 * a.round() as u64 + b.round() as u64)
+        .filter(|x| is_close(x[0]) && is_close(x[1]))
+        .map(|x| 3 * x[0].round() as u64 + x[1].round() as u64)
         .sum()
 }
 
