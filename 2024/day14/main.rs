1a7aa9aaa1cae28d61e466469e2cd0346d91e6b0
Danny Nguyen <danny@keeb.io>
2024-12-14 16:25:01 -0500

    Complete 2024.14.2

--- 2024/day14/main.rs
+++ 2024/day14/main.rs
@@ -52,11 +52,51 @@ fn part1(input: &str, size: Complex<i32>) -> u64 {
     count_frequencies(&quadrants).values().product::<u32>() as u64
 }
 
+fn print_points(points: &[Complex<i32>], size: Complex<i32>) {
+    for r in 0..size.re {
+        for c in 0..size.im {
+            if points.contains(&Complex::new(r, c)) {
+                print!("#");
+            } else {
+                print!(".");
+            }
+        }
+        println!();
+    }
+}
+
+fn part2(input: &str, size: Complex<i32>) -> u32 {
+    let robots = input
+        .lines()
+        .map(|l| {
+            let data = aoc2024_utils::extract_i32s(l);
+            let p = Complex::new(data[0], data[1]);
+            let v = Complex::new(data[2], data[3]);
+            Robot { p, v }
+        })
+        .collect_vec();
+
+    let mut time: i32 = 0;
+    loop {
+        let cur_pos = robots
+            .iter()
+            .map(|r| final_pos(r, time, size))
+            .collect_vec();
+
+        // Check if line 33 has a bunch of consecutive points in a row
+        if (21..40).all(|y| cur_pos.contains(&Complex::new(33, y))) {
+            print_points(&cur_pos, size);
+            return time as u32;
+        }
+        time += 1;
+    }
+}
+
 fn main() -> std::io::Result<()> {
     //let (input, size) = (include_str!("sample.txt"), Complex::new(11, 7));
     let (input, size) = (include_str!("input.txt"), Complex::new(101, 103));
 
     aoc2024_utils::timeit_u64("Part 1", || part1(input, size));
-    //aoc2024_utils::timeit_u64("Part 2", || part2(input));
+    aoc2024_utils::timeit("Part 2", || part2(input, size));
     Ok(())
 }
