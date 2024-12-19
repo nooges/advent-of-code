3299d65a78a9b1d493b44f77c1d57713f42052f6
Danny Nguyen <danny@keeb.io>
2024-12-19 12:57:31 -0500

    Complete 2024.19.2

--- 2024/day19/main.rs
+++ 2024/day19/main.rs
@@ -1,6 +1,5 @@
 use cached::proc_macro::cached;
 use itertools::Itertools;
-use rustc_hash::FxHashMap as HashMap;
 use rustc_hash::FxHashSet as HashSet;
 
 fn parse(input: &str) -> (HashSet<&str>, Vec<&str>) {
@@ -17,77 +16,41 @@ fn parse(input: &str) -> (HashSet<&str>, Vec<&str>) {
     (patterns, designs)
 }
 
-fn part1(patterns: HashSet<&str>, designs: Vec<&str>) -> u32 {
-    let mut possible = patterns.clone();
-    let mut impossible = HashSet::default();
-    impossible.insert("bwgb");
-    impossible.insert("wwugbwgb");
-    impossible.insert("bggb");
-    impossible.insert("wbburwwbggbbgrrbbwwrruuwrrbuguwugwwugbwgb");
-    impossible.insert("bbrbgrgrbguurgwuurrgwuurbrgwrugwbrgwbbuwgbubrwugbggb");
-    impossible.insert("rgbugugurwgurrwuuuguggwggrbrbubgwrwuwbbwgbbgwuuugbggb");
-    impossible.insert("gbbwuwwbbgwurgwguuwrbgbbgbguruuurrrgbwugwwbrgrgwgrwugbbgb");
-
-    fn is_possible<'a>(
-        design: &'a str,
-        patterns: &HashSet<&str>,
-        possible: &mut HashSet<&'a str>,
-        impossible: &mut HashSet<&'a str>,
-    ) -> bool {
-        if design.is_empty() || possible.contains(design) {
-            return true;
-        }
-        if impossible.contains(design) {
-            return false;
-        }
-        println!("Checking: {design}");
-        let possible_prefixes = patterns
-            .iter()
-            .filter(|p| design.starts_with(*p))
-            .collect_vec();
-        if possible_prefixes.is_empty() {
-            impossible.insert(design);
-            return false;
-        }
-        let possible_suffixes = patterns
-            .iter()
-            .filter(|p| design.ends_with(*p))
-            .collect_vec();
-        if possible_suffixes.is_empty() {
-            impossible.insert(design);
-            return false;
-        }
-
-        let result = possible_prefixes.iter().try_for_each(|p| {
-            if is_possible(&design[p.len()..], patterns, possible, impossible) {
-                possible.insert(design);
-                println!("Inserting: {design}");
-                return None;
-            }
-            Some(())
-        });
-
-        result.is_none()
+#[cached(key = "String", convert = r#"{ design.to_string() }"#)]
+fn num_possibilities(design: &str, patterns: &HashSet<&str>) -> u64 {
+    if design.is_empty() {
+        return 1;
     }
+    let possible_prefixes = &patterns
+        .iter()
+        .filter(|p| design.starts_with(**p))
+        .collect_vec();
 
+    possible_prefixes
+        .iter()
+        .map(|d| num_possibilities(&design[d.len()..], patterns))
+        .sum()
+}
+
+fn part1(patterns: HashSet<&str>, designs: Vec<&str>) -> u64 {
     designs
         .iter()
-        .filter(|design| {
-            dbg!(is_possible(
-                design,
-                &patterns,
-                &mut possible,
-                &mut impossible
-            ))
-        })
-        .count() as u32
+        .filter(|design| dbg!(num_possibilities(design, &patterns)) > 0)
+        .count() as u64
+}
+
+fn part2(patterns: HashSet<&str>, designs: Vec<&str>) -> u64 {
+    designs
+        .iter()
+        .map(|design| num_possibilities(design, &patterns))
+        .sum::<u64>()
 }
 
 fn main() -> std::io::Result<()> {
-    let input = include_str!("sample.txt");
+    let input = include_str!("input.txt");
     let (patterns, designs) = parse(input);
 
-    aoc2024_utils::timeit("Part 1", || part1(patterns.clone(), designs.clone()));
-    //aoc2024_utils::timeit("Part 2", || part2(patterns.clone(), designs.clone()));
+    aoc2024_utils::timeit_u64("Part 1", || part1(patterns.clone(), designs.clone()));
+    aoc2024_utils::timeit_u64("Part 2", || part2(patterns.clone(), designs.clone()));
     Ok(())
 }
