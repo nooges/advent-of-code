5213283d0c3a6477c570cced62dfbc6b390e793a
Danny Nguyen <danny@keeb.io>
2025-12-03 10:52:28 -0500

    Complete 2025.3.2 and refactor

--- 2025/day3/main.rs
+++ 2025/day3/main.rs
@@ -7,31 +7,45 @@ fn parse_input(input: &str) -> Vec<Vec<u32>> {
         .collect()
 }
 
-fn part1(input: &str) -> u32 {
-    let data = parse_input(input);
+fn find_first_max(v: &[u32]) -> (usize, u32) {
+    let (idx, &val) = v
+        .iter()
+        .enumerate()
+        .rev() // Reverse because max_by_key finds the last max value
+        .max_by_key(|(_, val)| *val)
+        .unwrap();
+    (idx, val)
+}
+
+fn solve(data: Vec<Vec<u32>>, n: usize) -> u64 {
     data.iter()
         .map(|v| {
             let len = v.len();
-            let (max_i, max_left) = &v[0..len - 1]
-                .iter()
-                .enumerate()
-                .rev() // Reverse because max_by_key finds the last max value
-                .max_by_key(|(_, val)| **val)
-                .unwrap();
-            let max_right = &v[max_i + 1..].iter().max().unwrap();
-            10 * *max_left + *max_right
+            (0..n)
+                .rev()
+                .fold((0, 0), |(acc, pos), i| {
+                    let (max_i, d) = find_first_max(&v[pos..len - i]);
+                    (acc * 10 + d as u64, pos + max_i + 1)
+                })
+                .0
         })
         .sum()
 }
 
+fn part1(input: &str) -> u64 {
+    let data = parse_input(input);
+    solve(data, 2)
+}
+
 fn part2(input: &str) -> u64 {
-    0
+    let data = parse_input(input);
+    solve(data, 12)
 }
 
 fn main() -> std::io::Result<()> {
     let input = include_str!("input.txt");
 
-    aoc2025_utils::timeit("Part 1", || part1(input));
-    //aoc2025_utils::timeit_u64("Part 2", || part2(input));
+    aoc2025_utils::timeit_u64("Part 1", || part1(input));
+    aoc2025_utils::timeit_u64("Part 2", || part2(input));
     Ok(())
 }
