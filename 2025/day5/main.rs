a51f2812ca70b164cc83a81241769245947756b7
Danny Nguyen <danny@keeb.io>
2025-12-05 13:30:24 -0500

    Complete 2025.5.2

--- 2025/day5/main.rs
+++ 2025/day5/main.rs
@@ -8,6 +8,7 @@ fn parse_input(input: &str) -> (Vec<(u64, u64)>, Vec<u64>) {
             let (a, b) = l.split_once("-").unwrap();
             (a.parse().unwrap(), b.parse().unwrap())
         })
+        .sorted()
         .collect_vec();
     let ids = ids_str.lines().map(|l| l.parse().unwrap()).collect_vec();
     (ranges, ids)
@@ -15,20 +16,44 @@ fn parse_input(input: &str) -> (Vec<(u64, u64)>, Vec<u64>) {
 
 fn part1(input: &str) -> u32 {
     let (ranges, ids) = parse_input(input);
-    //println!("{ranges:?} {ids:?}");
     ids.iter()
         .filter(|id| ranges.iter().any(|(start, end)| *id >= start && *id <= end))
         .count() as u32
 }
 
-fn part2(input: &str) -> u32 {
-    0
+fn part2(input: &str) -> u64 {
+    let (ranges, _) = parse_input(input);
+    println!("{ranges:?}");
+
+    let mut i = 1;
+    let mut a = ranges[0];
+    let mut sum = 0;
+    loop {
+        let b = ranges[i];
+        if a.1 >= b.0 {
+            if a.1 < b.1 {
+                a = (a.0, b.1);
+            }
+        } else {
+            sum += a.1 - a.0 + 1;
+            a = b;
+        }
+        i += 1;
+        println!("i: {i}");
+        if i >= ranges.len() {
+            sum += a.1 - a.0 + 1;
+            break;
+        }
+    }
+    sum
 }
 
 fn main() -> std::io::Result<()> {
     let input = include_str!("input.txt");
 
     aoc2025_utils::timeit("Part 1", || part1(input));
-    //aoc2025_utils::timeit("Part 2", || part2(input));
+    aoc2025_utils::timeit_u64("Part 2", || part2(input));
     Ok(())
 }
+
+// Low: 348512121918910
