af7e6eb3785ff0f672c119b144b19e8b78c872a9
Danny Nguyen <danny@keeb.io>
2025-12-08 14:36:23 -0500

    Complete 2025.8.2

--- 2025/day8/main.rs
+++ 2025/day8/main.rs
@@ -61,9 +61,54 @@ fn part1(input: &str, num_connections: usize) -> u32 {
         .product()
 }
 
-fn part2(input: &str) -> u32 {
-    //let rows = parse_input(input);
-    0
+fn part2(input: &str) -> u64 {
+    let boxes = parse_input(input);
+    let distances = boxes
+        .iter()
+        .combinations(2)
+        .map(|combo| {
+            let (a_idx, a) = combo[0];
+            let (b_idx, b) = combo[1];
+            let dist = (a[0] - b[0]) * (a[0] - b[0])
+                + (a[1] - b[1]) * (a[1] - b[1])
+                + (a[2] - b[2]) * (a[2] - b[2]);
+            (dist, a_idx, b_idx)
+        })
+        .sorted_by_key(|(d, _, _)| *d)
+        .collect_vec();
+
+    let mut circuits: Vec<HashSet<usize>> = Vec::new();
+    let mut res = 0;
+
+    for (_, a, b) in &distances {
+        let a_idx = circuits.iter().position(|c| c.contains(a));
+        let b_idx = circuits.iter().position(|c| c.contains(b));
+        match (a_idx, b_idx) {
+            (Some(a_idx), Some(b_idx)) => {
+                if a_idx == b_idx {
+                    continue;
+                }
+
+                let circuit1 = circuits.remove(a_idx.max(b_idx));
+                let circuit2 = circuits.remove(a_idx.min(b_idx));
+                circuits.push(circuit1.union(&circuit2).cloned().collect());
+            }
+            (Some(a_idx), None) => {
+                circuits[a_idx].insert(**b);
+            }
+            (None, Some(b_idx)) => {
+                circuits[b_idx].insert(**a);
+            }
+            _ => {
+                circuits.push(HashSet::from_iter(vec![**a, **b]));
+            }
+        }
+        if circuits.len() == 1 && circuits[0].len() == boxes.len() {
+            res = boxes[**a].1[0] * boxes[**b].1[0];
+            break;
+        }
+    }
+    res
 }
 
 fn main() -> std::io::Result<()> {
@@ -71,6 +116,8 @@ fn main() -> std::io::Result<()> {
 
     //aoc2025_utils::timeit("Part 1", || part1(input, 10));
     aoc2025_utils::timeit("Part 1", || part1(input, 1000));
-    //aoc2025_utils::timeit("Part 2", || part2(input));
+    aoc2025_utils::timeit_u64("Part 2", || part2(input));
     Ok(())
 }
+
+// Low: 479575008
