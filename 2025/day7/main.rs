3097c1e874ba93fab18f3e113f66cf7745843dd8
Danny Nguyen <danny@keeb.io>
2025-12-09 08:31:42 -0500

    Complete 2025.7.2

--- 2025/day7/main.rs
+++ 2025/day7/main.rs
@@ -33,13 +33,27 @@ fn part1(input: &str) -> u32 {
 }
 
 fn part2(input: &str) -> u64 {
-    0
+    let rows = parse_input(input);
+    let init_col = *rows[0].iter().collect_vec()[0];
+    let num_cols = input.lines().next().unwrap().len();
+    let mut counts = vec![0; num_cols];
+    counts[init_col] = 1;
+    for r in &rows[2..] {
+        for &c in r.iter() {
+            if r.contains(&c) {
+                counts[c - 1] += counts[c];
+                counts[c + 1] += counts[c];
+                counts[c] = 0;
+            }
+        }
+    }
+    counts.iter().sum::<u64>()
 }
 
 fn main() -> std::io::Result<()> {
     let input = include_str!("input.txt");
 
     aoc2025_utils::timeit("Part 1", || part1(input));
-    //aoc2025_utils::timeit_u64("Part 2", || part2(input));
+    aoc2025_utils::timeit_u64("Part 2", || part2(input));
     Ok(())
 }
