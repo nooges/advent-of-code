0ab3511b25b668f3bfb7f21fd5b4b96380d4aa8b
Danny Nguyen <danny@keeb.io>
2025-12-11 12:04:06 -0500

    Replace manual cache with cache lib

--- 2025/day11/main.rs
+++ 2025/day11/main.rs
@@ -1,3 +1,4 @@
+use cached::proc_macro::cached;
 use itertools::Itertools;
 use rustc_hash::FxHashMap as HashMap;
 use rustc_hash::FxHashSet as HashSet;
@@ -8,7 +9,6 @@ fn parse_input(input: &str) -> HashMap<String, HashSet<String>> {
             .lines()
             .map(|l| {
                 let (name, outputs) = l.split_once(": ").unwrap();
-
                 (
                     name.to_string(),
                     outputs.split(" ").map(|s| s.to_string()).collect(),
@@ -18,30 +18,12 @@ fn parse_input(input: &str) -> HashMap<String, HashSet<String>> {
     )
 }
 
+#[cached(key = "String", convert = r#"{ format!("{}-{}", node_name, goal) }"#)]
 fn dfs(nodes: &HashMap<String, HashSet<String>>, node_name: &str, goal: &str) -> u64 {
-    dfs_helper(nodes, node_name, goal, &mut HashMap::default())
-}
-
-fn dfs_helper(
-    nodes: &HashMap<String, HashSet<String>>,
-    node_name: &str,
-    goal: &str,
-    mem: &mut HashMap<String, u64>,
-) -> u64 {
-    match mem.get(node_name) {
-        Some(v) => *v,
-        None => match node_name {
-            n if n == goal => 1,
-            "out" => 0,
-            _ => {
-                let sum = nodes[node_name]
-                    .iter()
-                    .map(|n| dfs_helper(nodes, n, goal, mem))
-                    .sum();
-                mem.insert(node_name.to_string(), sum);
-                sum
-            }
-        },
+    match node_name {
+        n if n == goal => 1,
+        "out" => 0,
+        _ => nodes[node_name].iter().map(|n| dfs(nodes, n, goal)).sum(),
     }
 }
 
@@ -52,11 +34,11 @@ fn part1(input: &str) -> u64 {
 
 fn part2(input: &str) -> u64 {
     let devices = parse_input(input);
-    let dac_to_fft = dfs(&devices, "dac", "fft");
-    if dac_to_fft == 0 {
-        dfs(&devices, "svr", "fft") * dfs(&devices, "fft", "dac") * dfs(&devices, "dac", "out")
+    let fft_to_dac = dfs(&devices, "fft", "dac");
+    if fft_to_dac != 0 {
+        dfs(&devices, "svr", "fft") * fft_to_dac * dfs(&devices, "dac", "out")
     } else {
-        dfs(&devices, "svr", "dac") * dac_to_fft * dfs(&devices, "fft", "out")
+        dfs(&devices, "svr", "dac") * dfs(&devices, "dac", "fft") * dfs(&devices, "fft", "out")
     }
 }
 
